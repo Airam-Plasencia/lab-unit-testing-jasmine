edc3430f991fc6b7478576bc523ebcf04f3a663f
Airam Plasencia <airaplas@gmail.com>
2025-01-17 19:47:18 +0000

    done

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -19,6 +19,11 @@ describe("Iteration 1 | Getting Started", () => {
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+      it ("hould return undefined if any of the arguments is not a number", ()=> {
+        expect(add(1,"10")).toEqual(undefined);
+        expect(add("101",100)).toEqual(undefined);
+        expect(add("1011" , "111")).toEqual(undefined);
+      })
     });
   });
   
